523cbf7b697d2b36d1f8f47bba0bcda035c4c1c3
Tong <cnlutong@gmail.com>
2023-11-24 23:53:33 +0100

    Frist Commit

---
+++ src/test/java/de/luandtong/nachtwaechter/NachtwaechterApplicationTests.java
@@ -0,0 +1,13 @@
+package de.luandtong.nachtwaechter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NachtwaechterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
